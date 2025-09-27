e0dd86ca4fc9915b581df4249a5c028565074afa
nabaraj-bhandari <nabarajbhandari2005@gmail.com>
2025-09-27 07:03:59 +0545

    changed config

--- dwm/config.h
+++ dwm/config.h
@@ -112,15 +112,10 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
 
     // TAG KEYS
-    TAGKEYS(XK_1, 0)
-    TAGKEYS(XK_2, 1)
-    TAGKEYS(XK_3, 2)
-    TAGKEYS(XK_4, 3)
-    TAGKEYS(XK_5, 4)
-    TAGKEYS(XK_6, 5)
-    TAGKEYS(XK_7, 6)
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
+        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6)
 
-    {MODKEY | ShiftMask, XK_q, quit, {0}},
+            {MODKEY | ShiftMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
