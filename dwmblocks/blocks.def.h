9f55550675556ea1f67f6e3dd2c28b21f6c93253
nabaraj-bhandari <nabarajbhandari2005@gmail.com>
2025-10-01 02:09:44 +0545

    removed dwm

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,22 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+// blocks.h
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+// Define the structure for a block
+typedef struct {
+  char *icon;    // optional icon/text
+  char *command; // shell command to run
+  int interval;  // in seconds (0 = once)
+  int signal;    // signal number for manual update
+} Block;
+
+// Define the blocks to show in the status bar
+static const Block blocks[] = {
+    //{ "", "~/.dotfiles/.config/bin/music.sh",        1,      0 },
+    {" ", "~/.dotfiles/.config/bin/wifi.sh", 1, 0},
+    {"\uf028 ", "pamixer --get-volume-human", 1, 10},
+    {"\uf185 ", "brightnessctl | awk '/Current/ {print $4}' | tr -d '()'", 1,
+     12},
+    {"\uf017 ", "date '+%a %b %d %I:%M %p '", 10, 13},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+#define DELIMITER " | "
+#define DELIMITERENDLEN 3
